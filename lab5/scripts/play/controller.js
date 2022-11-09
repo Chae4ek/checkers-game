5f8718494c8af6fdb2a84b45072f38cdd55a87fe
Chae4ek <nominas.alexsey@gmail.com>
2022-11-09 20:37:02 +0600

    Add lab5

---
+++ lab5/scripts/play/controller.js
@@ -0,0 +1,275 @@
+class ChessboardController {
+  /**
+   * @param {ChessboardModel} chessboardModel
+   * @param {ChessboardView} chessboardView
+   */
+  constructor(chessboardModel, chessboardView) {
+    this.chessboardModel = chessboardModel;
+    this.chessboardView = chessboardView;
+    this.selectedField = null;
+    chessboardView.setClickOnFieldListener(this);
+  }
+
+  /**
+   * Calls when mouse clicks on field at (row, column) position
+   */
+  clickOnFieldEvent(row, column) {
+    if (!this.chessboardView.isSelectableField(row, column)) return;
+
+    const clickedField = this.chessboardModel.getField(row, column);
+
+    if (this.selectedField === clickedField) this.#unsetSelectedField();
+    else if (this.selectedField === null) this.#setSelectedField(clickedField);
+    else this.#changeSelectedField(clickedField);
+  }
+
+  #unsetSelectedField() {
+    const clickedField = this.selectedField;
+
+    this.#disableHints(clickedField);
+
+    if (this.chessboardModel.isNowChainAttack()) {
+      this.chessboardView.setSelectableField(clickedField.row, clickedField.column, true);
+    } else {
+      this.#toggleSelectableFieldsForCurrentPlayer(true);
+    }
+  }
+
+  #setSelectedField(clickedField) {
+    this.#toggleSelectableFieldsForCurrentPlayer(false);
+    this.#enableHints(clickedField);
+  }
+
+  #changeSelectedField(clickedField) {
+    const moveFromField = this.selectedField;
+    this.#disableHints(this.selectedField);
+    this.#makeMove(moveFromField, clickedField);
+  }
+
+  #disableHints(clickedField) {
+    this.selectedField = null;
+    this.#toggleHints(clickedField, false);
+  }
+
+  #enableHints(clickedField) {
+    this.selectedField = clickedField;
+    this.#toggleHints(this.selectedField, true);
+  }
+
+  /**
+   * @param {Field} fromField
+   * @param {Field} toField
+   */
+  #makeMove(fromField, toField) {
+    const move = this.chessboardModel.tryMove(fromField, toField);
+    if (move === null) return;
+
+    this.#renderMove(move);
+    this.chessboardView.setHistoryText(this.chessboardModel.moveHistory.convertToString());
+
+    const willThatChainMove = this.#willThatChainMove(move);
+    if (willThatChainMove) this.#enableHints(toField);
+
+    this.chessboardView.toggleCancelButton(true);
+    if (this.#isThisEndMove(willThatChainMove)) this.chessboardView.toggleEndButton(true);
+  }
+
+  #renderMove(move) {
+    this.#renderPiece(null, move.fromField.row, move.fromField.column);
+    this.#renderPiece(move.toField.piece, move.toField.row, move.toField.column);
+    if (move.attackedPiece !== null) {
+      this.#renderPiece(null, move.attackedField.row, move.attackedField.column);
+    }
+  }
+
+  #willThatChainMove(move) {
+    if (!this.chessboardModel.rules.canAttackChainMove || move.attackedPiece === null) return false;
+
+    const movesFromDestination = this.chessboardModel.getPossibleMoves(move.toField.row, move.toField.column);
+    if (movesFromDestination.has(MoveType.ATTACK)) return true;
+
+    return false;
+  }
+
+  #isThisEndMove(willThatChainMove) {
+    return !willThatChainMove || this.chessboardModel.rules.canInterruptChainMove;
+  }
+
+  clickOnCancelButton() {
+    if (this.selectedField !== null) this.#disableHints(this.selectedField);
+
+    const lastMove = this.chessboardModel.undoMove();
+    if (lastMove === null) return;
+
+    this.#renderUndoMove(lastMove);
+    this.chessboardView.setHistoryText(this.chessboardModel.moveHistory.convertToString());
+
+    if (this.chessboardModel.isNowChainAttack()) {
+      this.#enableHints(lastMove.fromField);
+      if (!this.chessboardModel.rules.canInterruptChainMove) this.chessboardView.toggleEndButton(false);
+    } else {
+      this.chessboardView.setSelectableField(lastMove.toField.row, lastMove.toField.column, false);
+      this.#toggleSelectableFieldsForCurrentPlayer(true);
+      this.chessboardView.toggleMoveButtons(false);
+    }
+  }
+
+  #renderUndoMove(lastMove) {
+    this.#renderPiece(null, lastMove.toField.row, lastMove.toField.column);
+    this.#renderPiece(lastMove.pieceToMove, lastMove.fromField.row, lastMove.fromField.column);
+    if (lastMove.attackedPiece !== null) {
+      this.#renderPiece(lastMove.attackedPiece, lastMove.attackedField.row, lastMove.attackedField.column);
+    }
+  }
+
+  clickOnEndButton() {
+    this.chessboardView.toggleMoveButtons(false);
+    this.#finishPlayerTurn();
+    this.chessboardView.setGameInfoText(this.#getGameInfoString());
+  }
+
+  #getGameInfoString() {
+    let currentPlayerHasMoves = false;
+    this.chessboardModel.forEachField((field) => {
+      if (field.piece !== null && field.piece.color === this.chessboardModel.currentPlayerColor) {
+        if (this.chessboardModel.getPossibleMoves(field.row, field.column).size !== 0) {
+          currentPlayerHasMoves = true;
+          return false;
+        }
+      }
+      return true;
+    });
+
+    if (!currentPlayerHasMoves) {
+      if (this.chessboardModel.currentPlayerColor === PieceColor.WHITE) return "Игра окончена: чёрные победили";
+      return "Игра окончена: белые победили";
+    }
+
+    if (this.chessboardModel.currentPlayerColor === PieceColor.WHITE) return "Ход белых";
+    return "Ход чёрных";
+  }
+
+  #finishPlayerTurn() {
+    this.#toggleSelectableFieldsForCurrentPlayer(false);
+    this.chessboardModel.finishPlayerTurn();
+    this.#toggleSelectableFieldsForCurrentPlayer(true);
+  }
+
+  #toggleSelectableFieldsForCurrentPlayer(enableSelectable) {
+    this.chessboardModel.forEachField((field) => {
+      if (field.piece !== null && field.piece.color === this.chessboardModel.currentPlayerColor) {
+        this.chessboardView.setSelectableField(field.row, field.column, enableSelectable);
+      }
+      return true;
+    });
+  }
+
+  /**
+   * Toggles visual hints for specified field based on possible moves from it
+   */
+  #toggleHints(field, enableHints) {
+    this.chessboardView.setFieldType(field.row, field.column, enableHints ? FieldType.SELECTED : FieldType.NONE);
+    const moves = this.chessboardModel.getPossibleMoves(field.row, field.column);
+
+    for (const [type, typedMoves] of moves) {
+      const fieldType = type === MoveType.SILENT ? FieldType.SILENT_MOVE : FieldType.ATTACK_MOVE;
+      for (const move of typedMoves) {
+        this.chessboardView.setFieldType(
+          move.toField.row,
+          move.toField.column,
+          enableHints ? fieldType : FieldType.NONE
+        );
+      }
+    }
+  }
+
+  /**
+   * @see {@link ChessboardModel.setBoard}
+   * @param {string} FAN
+   */
+  setChessboard(FAN) {
+    if (this.selectedField !== null) this.#disableHints(this.selectedField);
+    this.#toggleSelectableFieldsForCurrentPlayer(false);
+    this.chessboardView.setHistoryText(null);
+    this.chessboardView.toggleMoveButtons(false);
+
+    this.chessboardModel.setBoard(FAN);
+    const board = this.chessboardModel.board;
+    for (let row = 0; row < board.length; ++row) {
+      for (let column = 0; column < board[0].length; ++column) {
+        this.#renderPiece(board[row][column].piece, row, column);
+      }
+    }
+
+    this.chessboardModel.currentPlayerColor = PieceColor.BLACK; // TODO: specify in FAN param whose turn it is now
+    this.#toggleSelectableFieldsForCurrentPlayer(false);
+    this.chessboardModel.currentPlayerColor = PieceColor.WHITE;
+    this.#toggleSelectableFieldsForCurrentPlayer(true);
+
+    this.chessboardView.setGameInfoText(this.#getGameInfoString());
+  }
+
+  #renderPiece(piece, row, column) {
+    if (piece === null) this.chessboardView.setEmptyField(row, column);
+    else if (piece instanceof Pawn) {
+      if (piece.color === PieceColor.BLACK) this.chessboardView.setBlackPawn(row, column);
+      else this.chessboardView.setWhitePawn(row, column);
+    } else {
+      if (piece.color === PieceColor.BLACK) this.chessboardView.setBlackQueen(row, column);
+      else this.chessboardView.setWhiteQueen(row, column);
+    }
+  }
+
+  clickOnShowButton() {
+    const historyText = this.chessboardView.getHistoryText();
+    this.setChessboard("-p-p-p-pp-p-p-p--p-p-p-p----------------P-P-P-P--P-P-P-PP-P-P-P-");
+    let word = "";
+    for (let i = 0; i < historyText.length; ++i) {
+      const char = historyText[i];
+      if (char !== " " && char !== "\t" && char !== "\n") word += char;
+      else {
+        if (word === "") continue;
+        const processed = this.#handleWord(word);
+        if (!processed) return;
+        word = "";
+      }
+    }
+    if (word !== "") this.#handleWord(word);
+  }
+
+  /**
+   * @param {string} word
+   * @returns true if the word was processed, false if error occurred
+   */
+  #handleWord(word) {
+    // examples: a2-F4:d5 | H8:f1
+    const canWordBeAMove = /[a-hA-H][1-8]([-:][a-hA-H][1-8]){1,}$/.test(word);
+    if (!canWordBeAMove) {
+      this.#showErrorWord(word);
+      return false;
+    }
+
+    for (let i = 0; i < word.length; i += 3) {
+      const column = word[i].toLowerCase().charCodeAt(0) - "a".charCodeAt(0);
+      const row = 7 - (word.charCodeAt(i + 1) - "1".charCodeAt(0));
+
+      if (!this.chessboardView.isSelectableField(row, column)) {
+        this.#showErrorWord(word);
+        return false;
+      }
+      this.clickOnFieldEvent(row, column);
+    }
+    this.clickOnEndButton();
+
+    return true;
+  }
+
+  #showErrorWord(word) {
+    this.chessboardView.setGameInfoText(`Ошибка хода: ${word}`);
+  }
+
+  copyMoveHistoryToClipboard() {
+    const moveHistory = this.chessboardModel.moveHistory.convertToString();
+    navigator.clipboard.writeText(moveHistory);
+  }
+}
---
+++ lab5/scripts/play/engine.js
@@ -0,0 +1,377 @@
+class Rules {
+  /**
+   * @param {boolean} canAttackMoveBackward whether a simple piece can make an attacking move backwards
+   * @param {boolean} isAttackMandatory whether an attack move is mandatory
+   * @param {boolean} firstPlayerIsWhite whether the first player is white, else is black
+   * @param {boolean} canAttackChainMove whether chain move is allowed, else only simple moves even if a chain move is possible
+   * @param {boolean} canInterruptChainMove whether you can finish your turn in the middle of a chain move
+   */
+  constructor(canAttackMoveBackward, isAttackMandatory, firstPlayerIsWhite, canAttackChainMove, canInterruptChainMove) {
+    this.canAttackMoveBackward = canAttackMoveBackward;
+    this.isAttackMandatory = isAttackMandatory;
+    this.firstPlayerIsWhite = firstPlayerIsWhite;
+    this.canAttackChainMove = canAttackChainMove;
+    this.canInterruptChainMove = canInterruptChainMove;
+  }
+}
+
+class ChessboardModel {
+  /**
+   * @param {Rules} rules
+   */
+  constructor(rules) {
+    this.rules = rules;
+    this.board = [];
+    for (let row = 0; row < 8; ++row) {
+      this.board[row] = [];
+      for (let column = 0; column < 8; ++column) {
+        this.board[row].push(new Field(row, column, this));
+      }
+    }
+    this.moveHistory = new MoveHistory();
+    if (this.rules.firstPlayerIsWhite) this.firstPlayerColor = PieceColor.WHITE;
+    else this.firstPlayerColor = PieceColor.BLACK;
+    this.currentPlayerColor = this.firstPlayerColor;
+  }
+
+  /**
+   * @param {Field} fromField
+   * @param {Field} toField
+   * @returns {Move?} move that was made or null if move is incorrect
+   */
+  tryMove(fromField, toField) {
+    const move = this.#createMove(fromField, toField);
+    if (move !== null) this.#makeMove(move);
+    return move;
+  }
+
+  finishPlayerTurn() {
+    if (this.currentPlayerColor === PieceColor.WHITE) this.currentPlayerColor = PieceColor.BLACK;
+    else this.currentPlayerColor = PieceColor.WHITE;
+  }
+
+  /**
+   * Undo last move and returns it
+   *
+   * @returns {Move?} previous move or null if none
+   */
+  undoMove() {
+    const lastMove = this.moveHistory.moves.pop();
+    lastMove.toField.piece = null;
+    lastMove.fromField.piece = lastMove.pieceToMove;
+    lastMove.pieceToMove.field = lastMove.fromField;
+    if (lastMove.attackedPiece !== null) lastMove.attackedField.piece = lastMove.attackedPiece;
+    return lastMove;
+  }
+
+  /**
+   * @returns {boolean} true if current move is chain attack
+   */
+  isNowChainAttack() {
+    if (!this.rules.canAttackChainMove) return false;
+    const lastMove = this.moveHistory.moves[this.moveHistory.moves.length - 1];
+    return lastMove !== undefined && this.currentPlayerColor === lastMove.pieceToMove.color;
+  }
+
+  /**
+   * @returns {Move?} a move or null if the move is incorrect
+   */
+  #createMove(fromField, toField) {
+    const createdMove = new Move(fromField, toField);
+    for (const [, moves] of this.getPossibleMoves(fromField.row, fromField.column)) {
+      for (const move of moves) {
+        if (move.toField === toField) {
+          createdMove.attackedField = move.attackedField;
+          createdMove.attackedPiece = move.attackedPiece;
+          return createdMove;
+        }
+      }
+    }
+    return null;
+  }
+
+  #makeMove(move) {
+    let piece = move.fromField.piece;
+    piece.field = move.toField;
+    if (piece instanceof Pawn) {
+      if (piece.color === PieceColor.WHITE && move.toField.row === 0) piece = new Queen(move.toField, piece.color);
+      if (piece.color === PieceColor.BLACK && move.toField.row === this.board[0].length - 1)
+        piece = new Queen(move.toField, piece.color);
+    }
+    move.toField.piece = piece;
+
+    move.fromField.piece = null;
+    if (move.attackedPiece !== null) move.attackedField.piece = null;
+
+    this.moveHistory.moves.push(move);
+    return move;
+  }
+
+  /**
+   * '-' == empty field
+   * 'p' == black pawn
+   * 'P' == white pawn
+   * 'q' == black queen
+   * 'Q' == white queen
+   *
+   * @param {string} FAN notation of figure placement by rows. Starts from A8 to H1
+   */
+  setBoard(FAN) {
+    if (FAN.length !== this.board.length * this.board[0].length) {
+      throw new Error(`FAN size (${FAN.length}) is not equal board size (64)`);
+    }
+    this.moveHistory = new MoveHistory();
+
+    for (let i = 0; i < FAN.length; ++i) {
+      const row = Math.floor(i / 8);
+      const column = i % 8;
+      switch (FAN[i]) {
+        case "-":
+          this.board[row][column].piece = null;
+          break;
+        case "p":
+          this.board[row][column].piece = new Pawn(this.board[row][column], PieceColor.BLACK);
+          break;
+        case "P":
+          this.board[row][column].piece = new Pawn(this.board[row][column], PieceColor.WHITE);
+          break;
+        case "q":
+          this.board[row][column].piece = new Queen(this.board[row][column], PieceColor.BLACK);
+          break;
+        case "Q":
+          this.board[row][column].piece = new Queen(this.board[row][column], PieceColor.WHITE);
+          break;
+        default:
+          throw new Error(`Incorrect FAN. Unknown character: ${FAN[i]}`);
+      }
+    }
+  }
+
+  /**
+   * @returns {Map<MoveType, Move[]>} allowed fields to move from specified position
+   */
+  getPossibleMoves(row, column) {
+    const moves = this.#getPossibleMovesWithoutMandatoryAttackRule(row, column);
+
+    if (this.isNowChainAttack()) {
+      moves.delete(MoveType.SILENT);
+      return moves;
+    }
+
+    if (this.rules.isAttackMandatory) {
+      if (!moves.has(MoveType.SILENT)) return moves;
+      this.forEachField((field) => {
+        if (field.piece !== null && field.piece.color === this.board[row][column].piece.color) {
+          if (this.#getPossibleMovesWithoutMandatoryAttackRule(field.row, field.column).has(MoveType.ATTACK)) {
+            moves.delete(MoveType.SILENT);
+            return false;
+          }
+        }
+        return true;
+      });
+    }
+
+    return moves;
+  }
+
+  #getPossibleMovesWithoutMandatoryAttackRule(row, column) {
+    return this.board[row][column].piece === null ? new Map() : this.board[row][column].piece.getPossibleMoves();
+  }
+
+  /**
+   * @returns {Field?} field if its coords are correct
+   */
+  getField(row, column) {
+    if (row >= 0 && row < this.board.length && column >= 0 && column < this.board[0].length)
+      return this.board[row][column];
+    return null;
+  }
+
+  /**
+   * @param {(Field) => boolean} action if it returns false the iteration will stop
+   */
+  forEachField(action) {
+    for (let row = 0; row < this.board.length; ++row) {
+      for (let column = 0; column < this.board[0].length; ++column) {
+        if (!action(this.board[row][column])) return;
+      }
+    }
+  }
+}
+
+class Field {
+  /**
+   * @param {number} row
+   * @param {number} column
+   * @param {ChessboardModel} chessboardModel
+   */
+  constructor(row, column, chessboardModel) {
+    this.row = row;
+    this.column = column;
+    this.chessboardModel = chessboardModel;
+    this.piece = null;
+  }
+}
+
+const MoveType = { SILENT: 0, ATTACK: 1 };
+const PieceColor = { BLACK: 0, WHITE: 1 };
+
+class Piece {
+  /**
+   * @param {Field} field
+   * @param {PieceColor} color
+   */
+  constructor(field, color) {
+    this.field = field;
+    this.color = color;
+  }
+
+  getPossibleMoves() {
+    return new Map();
+  }
+}
+
+function addAllowedMove(allowedMoves, moveType, move) {
+  let list = allowedMoves.get(moveType);
+  if (list === undefined) allowedMoves.set(moveType, (list = []));
+  list.push(move);
+}
+
+class Pawn extends Piece {
+  getPossibleMoves() {
+    const allowedMoves = new Map();
+    const colorCoeff = this.color === PieceColor.BLACK ? 1 : -1;
+    this.#tryAddAttackMove(colorCoeff, -1, allowedMoves);
+    this.#tryAddAttackMove(colorCoeff, 1, allowedMoves);
+    if (this.field.chessboardModel.rules.canAttackMoveBackward) {
+      this.#tryAddAttackMove(-colorCoeff, -1, allowedMoves);
+      this.#tryAddAttackMove(-colorCoeff, 1, allowedMoves);
+    }
+    if (allowedMoves.size === 0 || !this.field.chessboardModel.rules.isAttackMandatory) {
+      this.#tryAddSilentMove(colorCoeff, -1, allowedMoves);
+      this.#tryAddSilentMove(colorCoeff, 1, allowedMoves);
+    }
+    return allowedMoves;
+  }
+
+  #tryAddSilentMove(rowVector, columnVector, allowedMoves) {
+    const toField = this.field.chessboardModel.getField(this.field.row + rowVector, this.field.column + columnVector);
+    if (toField !== null && toField.piece === null) {
+      const move = new Move(this.field, toField);
+      addAllowedMove(allowedMoves, MoveType.SILENT, move);
+    }
+  }
+
+  #tryAddAttackMove(rowVector, columnVector, allowedMoves) {
+    const attackedField = this.field.chessboardModel.getField(
+      this.field.row + rowVector,
+      this.field.column + columnVector
+    );
+    if (attackedField !== null && attackedField.piece !== null && attackedField.piece.color !== this.color) {
+      const toField = this.field.chessboardModel.getField(
+        this.field.row + rowVector + rowVector,
+        this.field.column + columnVector + columnVector
+      );
+      if (toField !== null && toField.piece === null) {
+        const move = new Move(this.field, toField);
+        move.attackedField = attackedField;
+        move.attackedPiece = attackedField.piece;
+        addAllowedMove(allowedMoves, MoveType.ATTACK, move);
+      }
+    }
+  }
+}
+
+class Queen extends Piece {
+  getPossibleMoves() {
+    const allowedMoves = new Map();
+    this.#tryRayCastAndAddMoves(1, 1, allowedMoves);
+    this.#tryRayCastAndAddMoves(1, -1, allowedMoves);
+    this.#tryRayCastAndAddMoves(-1, 1, allowedMoves);
+    this.#tryRayCastAndAddMoves(-1, -1, allowedMoves);
+    if (this.field.chessboardModel.rules.isAttackMandatory && allowedMoves.has(MoveType.ATTACK)) {
+      allowedMoves.delete(MoveType.SILENT);
+    }
+    return allowedMoves;
+  }
+
+  #tryRayCastAndAddMoves(rowVector, columnVector, allowedMoves) {
+    let row = this.field.row + rowVector,
+      column = this.field.column + columnVector;
+    let toField = this.field.chessboardModel.getField(row, column);
+    let move = new Move(this.field, toField);
+    while (toField !== null) {
+      if (toField.piece !== null) {
+        if (move.attackedPiece !== null || toField.piece.color === this.color) return;
+        move.attackedField = toField;
+        move.attackedPiece = toField.piece;
+      } else {
+        move.toField = toField;
+        if (move.attackedPiece === null) addAllowedMove(allowedMoves, MoveType.SILENT, move);
+        else addAllowedMove(allowedMoves, MoveType.ATTACK, move);
+        const oldMove = move;
+        move = new Move(this.field, toField);
+        move.attackedField = oldMove.attackedField;
+        move.attackedPiece = oldMove.attackedPiece;
+      }
+      row += rowVector;
+      column += columnVector;
+      toField = this.field.chessboardModel.getField(row, column);
+    }
+  }
+}
+
+class Move {
+  /**
+   * @param {Field} fromField
+   * @param {Field} toField
+   */
+  constructor(fromField, toField) {
+    this.pieceToMove = fromField.piece;
+    this.fromField = fromField;
+    this.toField = toField;
+    this.attackedField = null;
+    this.attackedPiece = null;
+  }
+}
+
+class MoveHistory {
+  constructor() {
+    this.moves = new Array();
+  }
+
+  /**
+   * @returns {string}
+   */
+  convertToString() {
+    let historyString = "";
+    let prevMove = this.moves[0];
+    for (const move of this.moves) {
+      let part = "";
+
+      if (prevMove.toField !== move.fromField) {
+        if (move !== this.moves[0]) part += " ";
+        part += this.#fieldToString(move.fromField);
+      }
+
+      if (move.attackedPiece !== null) part += ":";
+      else part += "-";
+
+      part += this.#fieldToString(move.toField);
+
+      if (move.pieceToMove.color === PieceColor.WHITE) part = part.toUpperCase();
+
+      historyString += part;
+      prevMove = move;
+    }
+    return historyString;
+  }
+
+  /**
+   * @param {Field} field
+   * @returns {string}
+   */
+  #fieldToString(field) {
+    return String.fromCharCode("a".charCodeAt(0) + field.column) + (8 - field.row);
+  }
+}
